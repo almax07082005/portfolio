9b633a979fc87afebe5e30ecabdeaef0a73dbaa7
almax07082005 <almaxim07082005@gmail.com>
2024-06-22 10:59:09 +0300

    fix: comic is working

--- comic.js
+++ comic.js
@@ -1,16 +1,15 @@
 function fetchComicId(email) {
     const url = new URL('https://fwd.innopolis.university/api/hw2');
-    url.search = new URLSearchParams({ email });
+    url.search = new URLSearchParams({email});
 
     return fetch(url)
         .then(response => response.text())
-        .then(text => text)
         .catch(error => console.error(error));
 }
 
 function fetchComicDetails(id) {
     const url = new URL('https://fwd.innopolis.university/api/comic');
-    url.search = new URLSearchParams({ id });
+    url.search = new URLSearchParams({id});
 
     return fetch(url)
         .then(response => response.json())
@@ -20,10 +19,21 @@ function fetchComicDetails(id) {
 async function action() {
     const id = await fetchComicId(document.getElementById('email').value);
     json = await fetchComicDetails(id);
-    document.getElementById('comic-title').innerText = json.title;
+    document.getElementById('comic-title').textContent = json.safe_title;
     document.getElementById('comic-image').src = json.img;
-    document.getElementById('comic-date').innerText = json.year;
+
+    const day = json.day;
+    const month = json.month;
+    const year = json.year;
+
+    const uploadDate = new Date(year, month - 1, day);
+    document.getElementById('comic-date').textContent = uploadDate.toLocaleDateString();
 }
 
-document.getElementById('comic-button').addEventListener('click', action);
-action();
+document.getElementById("comic-button").addEventListener("click", action);
+document.getElementById("email").addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+        event.preventDefault();
+        document.getElementById("comic-button").click();
+    }
+});
